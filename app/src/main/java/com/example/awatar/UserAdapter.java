a2fc851ef27efc8036503b2943a66b846691eca8
LordOwing <michallolo12345@gmail.com>
2025-10-28 19:06:19 +0100

    3

--- app/src/main/java/com/example/awatar/UserAdapter.java
+++ app/src/main/java/com/example/awatar/UserAdapter.java
@@ -9,6 +9,9 @@ import android.widget.TextView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
+import android.widget.Toast;
+
+import org.jspecify.annotations.NonNull;
 
 
 public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder> {
@@ -19,19 +22,28 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
         this.userList = userList;
     }
 
-
+    @NonNull
     @Override
-    public UserViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_user, parent, false);
         return new UserViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(UserViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
         User currentUser = userList.get(position);
         holder.firstNameText.setText(currentUser.getFirstName());
         holder.lastNameText.setText(currentUser.getLastName());
         holder.avatarImage.setImageResource(currentUser.getAvatarResId());
+
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = "Kliknięto: " + currentUser.getFirstName() + " " + currentUser.getLastName();
+                Toast.makeText(v.getContext(), message, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -44,7 +56,7 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
         public TextView firstNameText;
         public TextView lastNameText;
 
-        public UserViewHolder(View itemView) {
+        public UserViewHolder(@NonNull View itemView) {
             super(itemView);
             avatarImage = itemView.findViewById(R.id.avatarImage);
             firstNameText = itemView.findViewById(R.id.firstNameText);
